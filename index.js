215eb500a7ccd9a3b2a9fa34e53cfa1a1766d755
Lepos-L <56664500+Lepos-L@users.noreply.github.com>
2019-10-17 00:10:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,4 +9,49 @@ bot.on('ready', () => {
     console.log('The Wyvern is here!');
 })
 
+bot.on('message', msg=>{
+    let args = msg.content.substring(PREFIX.length).split(" ");
+     
+     switch(args[0]){
+         case 'hi':
+             msg.reply('greetings friend');
+             break;
+         case 'BB':
+             msg.channel.messageSend(https,//calendar.google.com/calendar/r/search?q=BB);
+             )
+             break;
+         case 'JB':
+             msg.channel.messageSend(https,//calendar.google.com/calendar/r/search?q=JB);
+             )
+             break;
+         case 'RF':
+             msg.channel.messageSend(https,//calendar.google.com/calendar/r/search?q=RF);
+             )
+             break;
+         case 'SP':
+             msg.channel.messageSend(https,//calendar.google.com/calendar/r/search?q=SP);
+             )
+             break;    
+         case 'SLD':
+             msg.channel.messageSend(https,//calendar.google.com/calendar/r/search?q=sld);
+             )
+             break;
+        case 'die5':
+            let replies = ["1", "2", "3", "4", "5"]
+            let results = Math.floor((Math.random() * replies.length))
+            msg.channel.messageSend(replies[result])  
+            break;             
+         case 'die6':
+             let replies = ["1", "2", "3", "4", "5", "6"]
+             let results = Math.floor((Math.random() * replies.length))
+             msg.channel.messageSend(replies[result])  
+             break;
+        case 'die10':
+             let replies = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+             let results = Math.floor((Math.random() * replies.length))
+             msg.channel.messageSend(replies[result])  
+             break;
+
+ }})
+
 bot.login(process.env.token);
