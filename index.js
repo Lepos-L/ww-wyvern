2faf28dc7f8309ea658bb7ec6ec9072a9a9e25ce
Lepos-L <56664500+Lepos-L@users.noreply.github.com>
2019-10-16 23:38:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
+const PREFIX = '-';
+
 const token = 'NjM0MDk4NjE3ODkxOTQ2NTI3.XaeEOQ.LV7v9XxDdYDbP4QsPMUjVhd4u7s';
 
 bot.on('ready', () => {
