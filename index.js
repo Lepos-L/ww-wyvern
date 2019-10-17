b86c37c5c1c4e6375d56575d8054cf62ebe3d6bb
Lepos-L <56664500+Lepos-L@users.noreply.github.com>
2019-10-17 00:13:31 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,52 +1,49 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-const PREFIX = '-';
-
 const token = 'NjM0MDk4NjE3ODkxOTQ2NTI3.XaeEOQ.LV7v9XxDdYDbP4QsPMUjVhd4u7s';
 
 bot.on('ready', () => {
     console.log('The Wyvern is here!');
 })
 
-bot.on('message', msg=>{
-    let args = msg.content.substring(PREFIX.length).split(" ");
+bot.on('message', msg => {
      
      switch(args[0]){
-         case 'hi':
+         case '-hi':
              msg.reply('greetings friend');
              break;
-         case 'BB':
+         case '-BB':
              msg.channel.messageSend(https,//calendar.google.com/calendar/r/search?q=BB);
              )
              break;
-         case 'JB':
+         case '-JB':
              msg.channel.messageSend(https,//calendar.google.com/calendar/r/search?q=JB);
              )
              break;
-         case 'RF':
+         case '-RF':
              msg.channel.messageSend(https,//calendar.google.com/calendar/r/search?q=RF);
              )
              break;
-         case 'SP':
+         case '-SP':
              msg.channel.messageSend(https,//calendar.google.com/calendar/r/search?q=SP);
              )
              break;    
-         case 'SLD':
+         case '-SLD':
              msg.channel.messageSend(https,//calendar.google.com/calendar/r/search?q=sld);
              )
              break;
-        case 'die5':
+        case '-die5':
             let replies = ["1", "2", "3", "4", "5"]
             let results = Math.floor((Math.random() * replies.length))
             msg.channel.messageSend(replies[result])  
             break;             
-         case 'die6':
+         case '-die6':
              let replies = ["1", "2", "3", "4", "5", "6"]
              let results = Math.floor((Math.random() * replies.length))
              msg.channel.messageSend(replies[result])  
              break;
-        case 'die10':
+        case '-die10':
              let replies = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
              let results = Math.floor((Math.random() * replies.length))
              msg.channel.messageSend(replies[result])  
